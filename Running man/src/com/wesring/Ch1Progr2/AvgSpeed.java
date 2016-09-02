dbd214fb7babae63f94b11aab16a99f07199ddf2
Wes Ring <wring436@gmail.com>
2016-09-01 22:29:55 -0400

    Comments

--- Running man/src/com/wesring/Ch1Progr2/AvgSpeed.java
+++ Running man/src/com/wesring/Ch1Progr2/AvgSpeed.java
@@ -3,11 +3,14 @@ package com.wesring.Ch1Progr2;
 //22.9 14
 public class AvgSpeed {
 	public static void main(String[] args){
-		double distanceRan = 24.0 * 1.6;
-		double timeRan = ((1*60)+(40)+(35.0/60.0))/60.0;
-		double answer = (distanceRan/timeRan);
-		String answerSTR = String.valueOf(answer);
 		
+		double distanceRan = 24.0 * 1.6; //Calculating the distance in km
+		double timeRan = ((1*60)+(40)+(35.0/60.0))/60.0; //Changing the time to hours
+		double answer = (distanceRan/timeRan); //Mathing it out
+		
+		//This next fancy bit with the string is here because Pearson and their REVEL software wouldn't accept an answer with more then 14 decimal places...
+		//By default the program outputs 15 decimal places.
+		String answerSTR = String.valueOf(answer);
 		System.out.println(answerSTR.substring(0,17));
 	}	
 }
