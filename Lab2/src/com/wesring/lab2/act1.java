27cfef9ba3291232a70616b1653a66ad47534573
Wes Ring <wring436@gmail.com>
2016-09-08 18:14:44 -0400

    We now have a clock

--- Lab2/src/com/wesring/lab2/act1.java
+++ Lab2/src/com/wesring/lab2/act1.java
@@ -1,5 +1,19 @@
 package com.wesring.lab2;
 
 public class act1 {
-
+	public static void main(String[] args) {
+		long milliseconds = System.currentTimeMillis();//
+		long seconds = milliseconds /1000;
+		long minutes = seconds / 60;
+		long hours = minutes / 60;
+		
+		System.out.println(milliseconds+ " Milliseconds"); //Display the milliseconds
+		System.out.println(seconds + " Seconds");//Display seconds
+		System.out.println(seconds % 60 + " Current Seconds");//Display current seconds
+		System.out.println(minutes + " Minutes");//Display Minutes
+		System.out.println(minutes % 60 + " Current Minutes");//Display current minutes
+		System.out.println(hours + " Hours");//Display hours
+		System.out.println(hours % 24 + " Current Hours");//Display current hours
+		System.out.println("\nThe time is " + (hours % 24) +":"+ (minutes % 60) +":"+ (seconds % 60));//Display current time
+	}
 }
