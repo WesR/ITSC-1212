33cc6060d126d80f8b5f22a9fd909d7928b49368
Wes Ring <wring436@gmail.com>
2016-09-08 17:37:40 -0400

    Making the formatting what they want

--- Lab1/src/com/wesring/lab1/Exer1.java
+++ Lab1/src/com/wesring/lab1/Exer1.java
@@ -1,9 +1,17 @@
 package com.wesring.lab1;
 
+/*
+ * Say what's gong to happen
+ * say the size of the rectangle
+ * Print out the math.
+ * @author Wes Ring
+ * @version 0.0.1
+ * @date 9/8/16
+ */
 public class Exer1 {
-	public static void main(String[] args){
-		System.out.println("This will caculate the area of a rectangle");//Preface
-		System.out.println("The width is 4.5 and Hight is 7.9"); //Description
-		System.out.println("The area is " +(4.5*7.9)); //Math 
+	public static void main(String[] args) {
+		System.out.println("This will caculate the area of a rectangle");// Preface
+		System.out.println("The width is 4.5 and Hight is 7.9"); // Description
+		System.out.println("The area is " + (4.5 * 7.9)); // Math
 	}
 }
--- Lab1/src/com/wesring/lab1/Exer2.java
+++ Lab1/src/com/wesring/lab1/Exer2.java
@@ -6,12 +6,15 @@ import java.util.Scanner;
  * Define: Input scanner
  * Prompt
  * Prompt,input and math
+ * @author Wes Ring
+ * @version 0.0.1
+ * @date 9/8/16
  */
 public class Exer2 {
 	public static void main(String[] args){
 			Scanner input = new Scanner(System.in);//Make the scanner
 			
-			System.out.println("What is your speed in kph?");//Formatting
+			System.out.println("What is your speed in kph?");//Formatting the prompt
 			System.out.print(">");
 			
 			System.out.println("Your going " + (input.nextInt() * 0.621) + " mph"); //Math and display
