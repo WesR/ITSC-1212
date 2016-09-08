5a5cffeffdb93d3448b23f09ff29d18d5e746f7e
Wes Ring <wring436@gmail.com>
2016-09-08 19:39:08 -0400

    Made a grade caculator

--- Lab2/src/com/wesring/lab2/act5.java
+++ Lab2/src/com/wesring/lab2/act5.java
@@ -1,5 +1,25 @@
 package com.wesring.lab2;
 
-public class act5 {
+import java.util.Scanner;
 
+/*
+ * @Author Wes Ring 
+ * @Version 0.0.1
+ * @Date 9/8/2016
+ */
+public class act5 {
+	public static void main(String[] args){
+		double inputOne, inputTwo;	//Define the vars
+		Scanner userInput = new Scanner(System.in); //Define the scanner
+		
+		System.out.print("Please input the number of questions missed \n>"); //Get questions missed
+		inputOne = userInput.nextDouble();
+		
+		System.out.print("Please input the total questions \n>"); //Get total questions
+		inputTwo = userInput.nextDouble();
+		
+		System.out.println("The student got a " + ((inputOne/inputTwo) * 100)+ "%");//Display the grade
+		
+		userInput.close();//Close the scanner
+	}
 }
