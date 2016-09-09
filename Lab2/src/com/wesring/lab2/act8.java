955f1195ee37f04ef552ae2c3921b2a7ca22a466
Wes Ring <wring436@gmail.com>
2016-09-09 16:30:04 -0400

    Error fixed a simple calculator

---
+++ Lab2/src/com/wesring/lab2/act8.java
@@ -0,0 +1,23 @@
+package com.wesring.lab2;
+
+import java.util.Scanner;//Imported scanner
+
+//I was told to fix the errors
+public class act8 {
+	public static void main(String[] args){   
+		int numPets1;   
+		int numPets2;   //Made this a int
+		int total;//Added a ;
+		Scanner scan = new Scanner(System.in);   
+		
+		System.out.println("How many pets do you own?");   
+		numPets1 = scan.nextInt();      
+		
+		System.out.print("How many pets does your partner own?");   
+		numPets2 = scan.nextInt();      
+		
+		total = numPets1 + numPets2;    //Removed the space between numPets and 1
+   
+		System.out.println("Together, you both have " + total + " pets");
+		}
+}
