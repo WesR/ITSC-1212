f0c60fcb65e48701bb71d3453733aa09465742fe
Wes Ring <wring436@gmail.com>
2016-09-09 16:32:04 -0400

    Graphical edits

--- Lab2/src/com/wesring/lab2/act8.java
+++ Lab2/src/com/wesring/lab2/act8.java
@@ -2,7 +2,9 @@ package com.wesring.lab2;
 
 import java.util.Scanner;//Imported scanner
 
-//I was told to fix the errors
+/*
+ * I was told to fix the errors
+ */
 public class act8 {
 	public static void main(String[] args){   
 		int numPets1;   
@@ -13,11 +15,11 @@ public class act8 {
 		System.out.println("How many pets do you own?");   
 		numPets1 = scan.nextInt();      
 		
-		System.out.print("How many pets does your partner own?");   
+		System.out.println("How many pets does your partner own?");//Made it start a new line, so it looked nicer.
 		numPets2 = scan.nextInt();      
 		
 		total = numPets1 + numPets2;    //Removed the space between numPets and 1
    
-		System.out.println("Together, you both have " + total + " pets");
+		System.out.println("Together, you both have " + total + " pets.");//Added a space so it would look better, and a period
 		}
 }
