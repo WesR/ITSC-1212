d4b20fdb4c2b448aee86369c576717f603702bf7
Wes Ring <wring436@gmail.com>
2016-09-14 17:48:13 -0400

    RevelWorkTest

--- REVEL Testing/src/com/wesring/reveltest/Main.java
+++ REVEL Testing/src/com/wesring/reveltest/Main.java
@@ -1,8 +1,22 @@
 package com.wesring.reveltest;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args){
-		int degreesCelsius, blahbla;
+		Scanner stdin = new Scanner(System.in);
+		int callsReceived;
+		int operatorsOnCall;
+		//callsReceived
+		//operatorsOnCall
+		//stdin
+		callsReceived = stdin.nextInt();
+		operatorsOnCall = stdin.nextInt();
+		if(operatorsOnCall == 0){
+			System.out.println("INVALID");
+		} else {
+			System.out.println(callsReceived / operatorsOnCall);
+		}
 		
 	}
 }
