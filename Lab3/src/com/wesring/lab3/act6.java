20d5d6c22d4e97f870a38e70879e76ef5d6c3f6c
Wes Ring <wring436@gmail.com>
2016-09-15 18:13:51 -0400

    Fixed more code

---
+++ Lab3/src/com/wesring/lab3/act6.java
@@ -0,0 +1,28 @@
+package com.wesring.lab3;
+
+/**
+ * * Description - This code is supposed to be the best. * It is a part of my
+ * job so I have to do it. Just writing these * comments because I have to. smh
+ * .. * Should hire someone to do it for me pls * @version 1.0 * @author Devansh
+ * Desai * @date July 16, 2016
+ */
+
+
+/*
+ * Code fixed by
+ * @Author Wes Ring 
+ * @Version 0.0.1
+ * @Date 9/15/2016
+ */
+
+public class act6 {
+	public static void main(String[] args) {
+		int score1, score2, score3;
+		double average;
+		score1 = 86;
+		score2 = 99;
+		score3 = 42;
+		average = ((score1 + score2 + score3) / 3.0);//Added () and .0
+		System.out.println("The average of 3 scores is : " + average);
+	}
+}
