c56925bfd8200405b4933ca133da2ac292f34dad
Wes Ring <wring436@gmail.com>
2016-09-15 17:44:16 -0400

    Made a discount and sales tax calculator

--- Lab3/src/com/wesring/lab3/act3.java
+++ Lab3/src/com/wesring/lab3/act3.java
@@ -1,7 +1,40 @@
 package com.wesring.lab3;
 
+import java.util.Scanner;
+
 public class act3 {
 	public static void main(String[] args) {
+		final double salesTax = 1.046; // Define the sales tax
+		Scanner userInput = new Scanner(System.in); // Define the scanner
+
+		double userBill;// Define the users bill
+		double userDiscount;// Define the users discount
+
+		System.out.print("Please input your bill total \n>"); // Prompt the user
+		userBill = userInput.nextDouble();
+
+		System.out.print("Please input your membership code \n>"); // Prompt the user
+		switch (userInput.nextInt()) {// Grab the user input
+		case 2014:
+			System.out.println("You get a 15% discount");
+			userDiscount = 0.85;
+			break;
+		case 2015:
+			System.out.println("You get a 10% discount");
+			userDiscount = 0.90;
+			break;
+		case 2016:
+			System.out.println("You get a 5% discount");
+			userDiscount = 0.95;
+			break;
+		default:
+			System.out.println("You get no discount");// just in case
+			userDiscount = 1.0;
+			break;
+		}
 		
+		System.out.println("Your total bill is $" + ((userBill*userDiscount)*salesTax));//Tell them the total
+		
+		userInput.close();// Closing the input
 	}
 }
