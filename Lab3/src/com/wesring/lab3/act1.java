278254d3eb9565c2a90f62cf82ed67c9a67abd92
Wes Ring <wring436@gmail.com>
2016-09-15 17:19:02 -0400

    Random if then selector

--- Lab3/src/com/wesring/lab3/act1.java
+++ Lab3/src/com/wesring/lab3/act1.java
@@ -1,5 +1,19 @@
 package com.wesring.lab3;
 
+/*
+ * @Author Wes Ring 
+ * @Version 0.0.1
+ * @Date 9/15/2016
+ */
 public class act1 {
-
+	public static void main(String[] args) {
+		System.out.println("Welcome to the special give away.");//Hello
+		
+		if(Math.random() > 0.5) { //Select a random option
+			System.out.println("You win 20% off");//20% off
+		} else {
+			System.out.println("You win 25% off");//25% off
+		}
+		
+	}
 }
