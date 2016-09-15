3505f18b14dec5cd7bb7d3d1897c9579d3eb48a1
Wes Ring <wring436@gmail.com>
2016-09-15 17:30:19 -0400

    User input in a switch statment

---
+++ Lab3/src/com/wesring/lab3/act2.java
@@ -0,0 +1,25 @@
+package com.wesring.lab3;
+
+import java.util.Scanner;
+
+public class act2 {
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in); // Define the scanner
+
+		System.out.print("Please input your membership code \n>"); //Prompt the user
+		switch (userInput.nextInt()) {//Grab the user input
+		case 2014:
+			System.out.println("You get a 15% discount");
+			break;
+		case 2015:
+			System.out.println("You get a 10% discount");
+			break;
+		case 2016:
+			System.out.println("You get a 5% discount");
+			break;
+		default:
+			System.out.println("You get no discount");//just in case
+			break;
+		}
+	}
+}
