b971dc8b069f14932e2d22e6b8ff1c8f1a07988b
Wes Ring <wring436@gmail.com>
2016-10-13 18:09:14 -0400

    A very simple chat bot
    It has no brain!

---
+++ Lab5/src/act4.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * @author Wes Ring 
+ * @Version 0.0.1
+ * @Date 10/13/2016
+ * 
+ * Logic: 
+ * 
+ */
+public class act4 {
+	public static void main(String[] args){
+		String userQuestion;
+		boolean chat = true;
+		Scanner userInput = new Scanner(System.in); //Define the scanner
+		
+		System.out.println("Hello, want to have a chat? Type a question! (Say goodbye to leave!)");
+		
+		do{
+			System.out.print(">");//Formatting user input
+			userQuestion = userInput.nextLine();//Get user Input
+			if (userQuestion.toLowerCase().equals("goodbye")){//Check for the leave flag
+				System.out.println("Goodbye! \nHave a nice day!");
+				chat = false;
+			} else {
+				System.out.println("Bot> " + botResponse());//Write a response
+			}
+			
+			
+		}while(chat);
+	}
+	
+	public static String botResponse(){
+		Random r = new Random();//We need a random number
+		
+		switch(r.nextInt(4)){//Pick a random response
+		case 0:
+			return "I dont know, what do you think?";
+		case 1: 
+			return "How cool!";
+		case 2: 
+			return "But why?";
+		case 3:
+			return "Ask NASA!";
+		case 4: 
+			return "Who knows!";
+		}
+		return "ERROR";
+	}
+}
