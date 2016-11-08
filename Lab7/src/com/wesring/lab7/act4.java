8e24299b7c79868f47e50596923cfebb817f084b
Wes Ring <wring436@gmail.com>
2016-11-08 00:30:22 -0500

    Added the lab activities and deck generator

--- Lab7/src/com/wesring/lab7/act4.java
+++ Lab7/src/com/wesring/lab7/act4.java
@@ -15,16 +15,18 @@ public class act4 {
 		String[] cardType = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen","King"};
 		String[] userDeck = generateUserDeck(cardSuit, cardType);
 		
-		System.out.println(Arrays.toString(userDeck));
+		System.out.println(Arrays.toString(userDeck));//Print the deck (Not required but helpful for debugging
 	}
 
 	private static String[] generateUserDeck(String[] cardSuit, String[] cardType) {
+		int tempPlace = 0;//To keep track of where we already have values
 		String[] userDeck = new String[52];
-		for(int i = 0; i < cardType.length; i++){
-			for(int k = 0; k < cardSuit.length; k++){
-				userDeck[i+k] = cardType[i] + " of " + cardSuit[k];
+		for(int i = 0; i < cardSuit.length; i++){//Rotate the card suit
+			for(int k = 0; k < cardType.length; k++){//Rotate the card type
+				userDeck[tempPlace] = cardType[k] + " of " + cardSuit[i];//Place it in the array
+				tempPlace++;//Move up an array index
 			}
 		}
-		return userDeck;
+		return userDeck;//Pass the deck back
 	}
 }
