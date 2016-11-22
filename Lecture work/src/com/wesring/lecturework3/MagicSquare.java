b3b097911bcc49265823580a3a5569f93265853f
Wes Ring <wring436@gmail.com>
2016-11-22 11:20:14 -0500

    Created the Magic Square class, and added the prompt

---
+++ Lecture work/src/com/wesring/lecturework3/MagicSquare.java
@@ -0,0 +1,14 @@
+package com.wesring.lecturework3;
+
+import java.util.Scanner;
+
+public class MagicSquare {
+	public static void main(String[] args){
+		Scanner userInput = new Scanner(System.in);//A scanner for user input
+		
+		System.out.println("Hello User,");
+		System.out.println("A Magic Square is a square that is divided into smaller squares, each containing a number, such that the figures in each vertical,\n horizontal, and diagonal row add up to the same value.");
+		System.out.println("You task is to try to create one, if you do correctly, I will tell you if it is one!");
+		System.out.println("Good Luck");
+	}
+}
